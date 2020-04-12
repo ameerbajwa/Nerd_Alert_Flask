ef331c51d8560614024a8895fbbbf01a3835155b
Ameer Bajwa <ameerbajwa@gmail.com>
2020-04-12 13:25:01 -0700

    Created a custom JSONEncoder class that will change the any datetime objects returned from the MySQL database to its isoformat style.
    Need to test DELETE API functional on postman.
    Started creating questions a HP1 book quiz .

--- main/app.py
+++ main/app.py
@@ -4,10 +4,13 @@ from flask_jwt import JWT
 
 from Resources import users, security, quiz, quiz_questions, user_quiz_results, user_question_results
 import config
+from customJSONEncoder import CustomJSONEncoder
 
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = config.APP_SECRET_KEY
+app.json_encoder = CustomJSONEncoder
+
 jwt = JWT(app, security.authenticate, security.identity)
 
 api.add_resource(users.UserRegister, '/register_user', methods=['POST'])
@@ -17,7 +20,7 @@ api.add_resource(quiz.GenerateQuiz, '/generate_quiz', methods=['POST'])
 api.add_resource(quiz.RetrieveQuiz, '/retrieve_quiz', methods=['POST'])
 
 api.add_resource(quiz_questions.GenerateQuizQuestions, '/generate_quiz_questions', methods=['POST'])
-api.add_resource(quiz.questions.RetrieveQuizQuestions, '/retrieve_quiz_questions', methods=['POST'])
+api.add_resource(quiz_questions.RetrieveQuizQuestions, '/retrieve_quiz_questions', methods=['POST'])
 
 api.add_resource(user_quiz_results.EnterUserQuizResults, '/enter_user_quiz_results', methods=['POST'])
 api.add_resource(user_question_results.EnterUserQuestionResults, '/enter_user_question_results', methods=['POST'])
